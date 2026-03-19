11ec86398b2a289abd59f0f2d131d95ac9b550f6
salimdehria <167587544+salimdehria@users.noreply.github.com>
2026-03-19 12:39:30 +0100

    Update Firebase configuration and initialization
    Updated Firebase configuration to include database URL and modified initialization code.

--- firebase.js
+++ firebase.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase.js - Configuration Firebase avec la bonne région
 const firebaseConfig = {
-  apiKey: "AIzaSyCUncMgmG6g3Xl7drWotu2v4zPaf8fCLP0",
-  authDomain: "aero-compition.firebaseapp.com",
-  projectId: "aero-compition",
-  storageBucket: "aero-compition.firebasestorage.app",
-  messagingSenderId: "717421617215",
-  appId: "1:717421617215:web:64d54ebb8bba5a52a6d88a",
-  measurementId: "G-QV1C0JHGP9"
+    apiKey: "AIzaSyCUncMgmG6g3Xl7drWotu2v4zPaf8fCLP0",
+    authDomain: "aero-compition.firebaseapp.com",
+    databaseURL: "https://aero-compition-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "aero-compition",
+    storageBucket: "aero-compition.firebasestorage.app",
+    messagingSenderId: "717421617215",
+    appId: "1:717421617215:web:64d54ebb8bba5a52a6d88a",
+    measurementId: "G-QV1C0JHGP9"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Initialiser Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const scoresRef = database.ref('competition/scores');
